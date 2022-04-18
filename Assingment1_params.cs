b7b4c9fce1e1175374b2743ecb1e461eeb2cf53e
Shivam Thorat <77840116+shivamthorat4@users.noreply.github.com>
2022-04-18 16:12:02 +0530

    Create Assingment1_params.cs

---
+++ Assingment1_params.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Console
+    {
+        class Static_Params
+        {
+            public static void Main(string[] args)
+            {
+
+                int[] n1 = new int[100];
+                int sum = 0;
+                for (int i = 1; i < 6; ++i)
+                {
+                    Console.WriteLine("Enter the value {0}:", i);
+                    n1[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                sum = SumOfInteger(n1);
+
+
+                Console.WriteLine("Result={0}", sum);
+                
+            }
+            static int SumOfInteger(params int[] val)
+            {
+                int sum = 0;
+                foreach (int i in val)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+
+
+    }
+
+}
