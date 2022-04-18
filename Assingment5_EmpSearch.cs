0b3587a4862ff4846c13a1287822e817c7871a07
Shivam Thorat <77840116+shivamthorat4@users.noreply.github.com>
2022-04-18 16:43:05 +0530

    Create Assingment5_EmpSearch.cs

---
+++ Assingment5_EmpSearch.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Search
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            list.Add("Shivam");
+            list.Add("Sanket");
+            list.Add("Rinku");
+            list.Add("Sagar");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("List of employees" + ':' + list[i]);
+            }
+            Console.WriteLine("total number of employess" + ':' + list.Count);
+            Console.WriteLine("enter person name:");
+            string X = Console.ReadLine();
+            if (list.Contains(X))
+            {
+                Console.WriteLine("yes " + X + " is an employee");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
