c075676cece90203614c7fd81a1694b8dc88d72a
Shivam Thorat <77840116+shivamthorat4@users.noreply.github.com>
2022-05-04 17:41:54 +0000

    Update Assingment1_swap.cs

--- Assingment1_swap.cs
+++ Assingment1_swap.cs
@@ -1,24 +1,23 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Swapping
 {
-    using System;
+  
     public class SwapNumber
     {
         public static void Main(string[] args)
         {
             int number1, number2, temp;
+            
             Console.Write("\nInput the First Number : ");
             number1 = int.Parse(Console.ReadLine());
+            
             Console.Write("\nInput the Second Number : ");
             number2 = int.Parse(Console.ReadLine());
+            
             temp = number1;
             number1 = number2;
             number2 = temp;
+            
             Console.Write("\nAfter Swapping : ");
             Console.Write("\nFirst Number : " + number1);
             Console.Write("\nSecond Number : " + number2);
